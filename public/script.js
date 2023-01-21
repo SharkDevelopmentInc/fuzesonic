15170e410880d60641502b4eb08de2be1525bd56
SharkDevelopemtInc <jaxhudnall@gmail.com>
2023-01-21 08:49:50 -0500

    e

---
+++ public/script.js
@@ -0,0 +1,8 @@
+/*r = Math.floor(Math.random() * 255) + 1;
+b = Math.floor(Math.random() * 255) + 1;
+g = Math.floor(Math.random() * 255) + 1;
+console.log(r, g, b);
+function pixelFiller(){
+document.getElementbyId("backgroundTag").style.background = rgb(r, g, b);
+}*/
+
